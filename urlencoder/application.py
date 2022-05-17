4cdd6c68114d1637e195fc0f42822f739754ce0c
mitty1293 <k.fmichi@gmail.com>
2022-05-17 09:00:20 +0900

    不要部分削除

--- urlencoder/application.py
+++ urlencoder/application.py
@@ -5,8 +5,6 @@ from logging import basicConfig
 from flask import Flask, render_template, request
 
 # Configure logging
-# If FLASK_ENV=production, then level=WARNING
-# If FLASK_ENV=development, then level=DEBUG
 basicConfig(
     filename="/app/logs/urlencoder.log",
     format="%(asctime)s\t%(levelname)s\t%(filename)s\t%(module)s\tline:%(lineno)d\t%(message)s",
@@ -31,16 +29,6 @@ def index():
         return render_template("index.html")
 
     # POST
-    # req_dict = request.form.to_dict()
-    # inputtext = req_dict.get("inputtext")
-    # if "encode_btn" in req_dict.keys():
-    #     # Encode
-    #     outputtext = urllib.parse.quote(inputtext, safe=":/")
-    # elif "decode_btn" in req_dict.keys():
-    #     # Decode
-    #     outputtext = urllib.parse.unquote(inputtext)
-    # return render_template("index.html", inputtext=inputtext, outputtext=outputtext)
-
     match request.form.to_dict():
         case {'inputtext': inputtext, 'encode_btn': encode_btn, 'outputtext': outputtext}:
             outputtext = urllib.parse.quote(inputtext, safe=":/")
